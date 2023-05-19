656b889f38d065b11abffde15ab9db0de25edf73
BuildTools <unconfigured@null.spigotmc.org>
2023-05-18 23:08:29 -0500

    v 0.0.0.1

---
+++ src/Router.ts
@@ -0,0 +1,39 @@
+
+import express,{Application,Request,Response} from 'express'
+
+
+
+//se crea una clase
+class App{
+    //Atributos publicos y privados
+    public app:any;
+    private server:any;
+
+    constructor(){
+        this.app=express();
+        this.app.use(express.json());
+        this.Routes()
+    }
+
+    private Routes():void{
+        this.app.get( 
+            "/",
+            (req:Request,res:Response)=>{
+                res.send("Holaaaaaa")
+            }
+        )
+    }
+
+    public start():void{
+        const port = 3000
+        this.app.listen(
+            port,()=>{`Server started in port ${port}`}
+        )
+    }
+
+    public close():void{
+        this.server.close()
+    }
+}
+
+export default App
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import App from "./Router";
+
+//se puede crear un objeto de la clase app
+
+const app= new App
+app.start()
